6fb920e9efd35b78c7f6961f083a39b2c20f4a0e
netpulse <njoshi@netpulse.com>
2016-09-05 14:39:05 -0700

    Added code to include duration when it is selected under set type

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,4 @@
-var routinesApp = angular.module("routinesApp", ["firebase", "npRoutinesDirectives.replbs", "npRoutinesDirectives.repbw", "npRoutinesDirectives.durationlbs", "npRoutinesDirectives.durationbw"]);
+var routinesApp = angular.module("routinesApp", ["firebase", "npRoutinesDirectives.replbs", "npRoutinesDirectives.repbw", "npRoutinesDirectives.durationlbs", "npRoutinesDirectives.durationbw", "npRoutinesDirectives.duration"]);
 
 //Use following url to connect with test DB on Firebase
 routinesApp.constant("FirebaseUrl", "https://planetf-clone.firebaseio.com/");
---
+++ app/js/directives/directives.duration.js
@@ -0,0 +1,45 @@
+var duration = angular.module("npRoutinesDirectives.duration", []);
+
+/*Contains all the directives to show Reps and Body Weight*/
+duration.directive("npdurationset", ["$compile", function($compile){
+    return {
+        restrict : "E",
+        replace : true,
+        template : `<div class="input-group">
+                        <span class="input-group-addon">Set</span>
+                        <input type="text" name="duration" id="duration" class="input-group" placeholder="Duration" pattern="[0-9]{2}[\:]{1}[0-9]{2}" required>
+                        <span class="input-group-addon">MM:SS</span>
+                    </div>`
+    }
+}]);
+
+duration.directive("addnewdurationsetbtn", ["$compile", function($compile){
+    return {
+        restrict : "E",
+        replace : true,
+        template : `<div class="form-inline">
+                        <div class="input-group">
+                            <button type="button" id="addDuration" name="addDuration" class="input-group" data-ng-click="getHTMLForDuration()" >Add New Set</button>
+                        </div>
+                    </div>`,
+        link : function(scope, element){
+            scope.getHTMLForDuration =  function(){
+                angular.element(document.getElementById("durationInputContainer")).append($compile("<br/><npdurationset></npdurationset>")(scope));
+            }
+        }
+    }
+}]);
+
+duration.directive("npdurationcontainer", ["$compile", function($compile){
+    return {
+        restrict : "E",
+        replace : true,
+        template : `<div id="durationContainer">
+                        <!--//Second line of form starts here-->
+                        <div class="form-inline" id="durationInputContainer">
+                            <npdurationset></npdurationset>
+                        </div>
+                        <addnewdurationsetbtn></addnewdurationsetbtn>
+                    </div>`
+    }
+}]);
