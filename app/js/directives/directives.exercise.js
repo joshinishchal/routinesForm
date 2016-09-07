0df7c6d7916c1408740ddf61f4aaf20b5a09583b
netpulse <njoshi@netpulse.com>
2016-09-07 16:03:26 -0700

    Added functionality to show different deta-list for different exercise in our list

--- app/js/directives/directives.exercise.js
+++ app/js/directives/directives.exercise.js
@@ -1,6 +1,6 @@
 var exerciseDir = angular.module("npRoutinesDirectives.exercise", []);
 
-exerciseDir.directive("npexercise", function(){
+exerciseDir.directive("npexercise", [function(){
     return{
         restrict : "E",
         replace : true,
@@ -10,9 +10,9 @@ exerciseDir.directive("npexercise", function(){
                         <!--//First line of form starts here-->
                         <div class="form-inline">
                             <div class="input-group exerciseSelector">
-                                <input type="text" class="input-group" name="exerciseType" id="exerciseType" list="exerciseTypes" placeholder="Select Type" data-ng-model="selectedExerciseType" required>
-                                <input type="text" class="input-group" name="exerciseName" id="exerciseName" list="exerciseNames" placeholder="Exercise Name" data-ng-model="selectedExerciseName" npcardioset={{getDurationDistanceVisibility(selectedExerciseType.toLowerCase(),selectedExerciseName,selectedSetType)}} exerciseType>
-                                <input type="text" class="input-group" name="setType" id="setType" list="setTypes" placeholder="Select Set Type" data-ng-model="selectedSetType" data-ng-show="isSetTypeVisible(selectedExerciseType.toLowerCase(), selectedExerciseName)" npsettype={{selectedSetType}} npcardioset={{getDurationDistanceVisibility(selectedExerciseType.toLowerCase(),selectedExerciseName,selectedSetType)}}>
+                                <input type="text" class="input-group" name="exerciseType" id="exerciseType" list="{{exerciseTypesId}}" placeholder="Select Type" data-ng-model="selectedExerciseType" required>
+                                <input type="text" class="input-group" name="exerciseName" id="exerciseName" list="{{exerciseNamesId}}" placeholder="Exercise Name" data-ng-model="selectedExerciseName" npcardioset={{getDurationDistanceVisibility(selectedExerciseType.toLowerCase(),selectedExerciseName,selectedSetType)}} exerciseType>
+                                <input type="text" class="input-group" name="setType" id="setType" list="{{setTypesId}}" placeholder="Select Set Type" data-ng-model="selectedSetType" data-ng-show="isSetTypeVisible(selectedExerciseType.toLowerCase(), selectedExerciseName)" npsettype={{selectedSetType}} npcardioset={{getDurationDistanceVisibility(selectedExerciseType.toLowerCase(),selectedExerciseName,selectedSetType)}}>
                             </div>
                             <div class="input-group cardioContainer">
                             </div>
@@ -24,13 +24,13 @@ exerciseDir.directive("npexercise", function(){
                             <div class="input-group delete-div">
                                 <button class="input-group delete-btn" type="button" id="deleteExercise" name="deleteExercise" deleteexercise>Delete</button>
                             </div>
-                            <datalist id="exerciseTypes">
+                            <datalist id="{{exerciseTypesId}}">
                                 <option data-ng-repeat="(exerciseType, exercise) in fbExerciseData['exerciseType']" data-value="{{fbExerciseData['exerciseCategories'][exerciseType]['name']}}">{{fbExerciseData['exerciseCategories'][exerciseType]['name']}}</option>
                             </datalist>
-                            <datalist id="exerciseNames">
+                            <datalist id="{{exerciseNamesId}}">
                                 <option data-ng-repeat="(exerciseName, subName) in getExerciserNameList(selectedExerciseType.toLowerCase(), fbExerciseData['exerciseType'])" data-value="{{exerciseName}}">{{exerciseName}}</option>
                             </datalist>
-                            <datalist id="setTypes">
+                            <datalist id="{{setTypesId}}">
                                 <option data-ng-repeat="(setType, attrs) in getSetType(selectedExerciseType.toLowerCase(), selectedExerciseName)" data-value="{{setType}}">{{setType}}</option>
                             </datalist>
                         </div>
@@ -39,9 +39,16 @@ exerciseDir.directive("npexercise", function(){
                             <!--<npreplbscontainer></npreplbscontainer>-->
                         </div>
                         <!--//Exercise Ends-->
-                    </div>`
+                    </div>`,
+        link : function(scope,element,attrs){
+            scope.$watch("idcounter", function(newValue, oldValue){
+                scope.exerciseTypesId = "exerciseTypes-" + attrs["idcounter"];
+                scope.exerciseNamesId = "exerciseNames-" + attrs["idcounter"];
+                scope.setTypesId = "setTypes-" + attrs["idcounter"];
+            }, true)
+        }
     }
-});
+}]);
 
 
 exerciseDir.directive("deleteexercise", [function(){
@@ -61,9 +68,10 @@ exerciseDir.directive("addnewexercise", ["$compile", function($compile){
         restrict : "A",
         replace : false,
         link : function(scope, element, attrs){
+            scope.idCounter = 0;
             element.on("click", function(){
-                console.log("onclick happened..");
-                angular.element(element.parent().parent().find("#exerciseContainer")).append($compile("<br/><npexercise></npexercise>")(scope));
+                angular.element(element.parent().parent().find("#exerciseContainer")).append($compile("<br/><npexercise idcounter=" + scope.idCounter + "></npexercise>")(scope));
+                scope.idCounter++;
             });
         }
     }
